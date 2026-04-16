e7d0ade7318753a561de0b1d35c0419fa0c90d9c
Ayush Chaubey <ayushchaubey19768@gmail.com>
2026-04-16 18:26:50 +0530

    Final update

--- server/app.js
+++ server/app.js
@@ -4,12 +4,24 @@ import bookingRoutes from "./routes/bookingRoutes.js";
 
 const app = express();
 
+// ✅ allowed origins
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://spa-home.vercel.app"
+];
+
 app.use(
     cors({
-        origin: [
-            "http://localhost:5173",
-            "https://spa-home.vercel.app/",
-        ],
+        origin: function (origin, callback) {
+            // allow requests with no origin (Postman, mobile apps)
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
     })
 );
